513c80f6a382e16f54a379e63313226f3ce600c1
Nihal Singh Gulati <nihalsgulati@gmail.com>
2026-04-05 18:13:41 -0700

    pusg

--- flight-software/src/tests/test-radio/main.cpp
+++ flight-software/src/tests/test-radio/main.cpp
@@ -1,10 +1,132 @@
-#include <Arduino.h>
+/*
+  LoRa Duplex communication
+
+  Sends a message every half second, and polls continually
+  for new incoming messages. Implements a one-byte addressing scheme,
+  with 0xFF as the broadcast address.
+
+  Uses readString() from Stream class to read payload. The Stream class'
+  timeout may affect other functuons, like the radio's callback. For an
+
+  created 28 April 2017
+  by Tom Igoe
+*/
+#include <Arduino.h>              // include libraries
+#include <SPI.h>
+#include <LoRa.h>
+
+const int csPin = A5;          // LoRa radio chip select
+const int resetPin = 7;       // LoRa radio reset
+const int irqPin = A4;         // change for your board; must be a hardware interrupt pin
+
+String outgoing;              // outgoing message
+
+byte msgCount = 0;            // count of outgoing messages
+byte localAddress = 0xBB;     // address of this device
+byte destination = 0xFF;      // destination to send to
+long lastSendTime = 0;        // last send time
+int interval = 1000;          // interval between sends
+void sendMessage(String outgoing);
+void onReceive(int packetSize);
+
+// void setup() {
+//   Serial.begin(115200);                   // initialize serial
+//   SPI.begin(5, 21, 19, 4);
+//   pinMode(csPin, OUTPUT);
+//   pinMode(resetPin, OUTPUT);
+//   digitalWrite(resetPin, LOW);
+//   delay(10);
+//   digitalWrite(resetPin, HIGH);
+//   delay(10);
+// }
+// void loop() {
+//   digitalWrite(csPin, LOW);
+//   byte ver = SPI.transfer(0x42);  // read version register
+//   digitalWrite(csPin, HIGH);
+//   Serial.println(ver, HEX);  // should be 0x12 for RFM95W/SX1276
+//   delay(1000);
+// }
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(115200);                   // initialize serial
+  while (!Serial);
+
+  pinMode(LED_BUILTIN, OUTPUT); // initialize LED digital pin as an output.
+  digitalWrite(LED_BUILTIN, LOW);
+  pinMode(csPin, OUTPUT);
+  pinMode(resetPin, OUTPUT);
+
+  Serial.println("LoRa Duplex");
+
+  // override the default CS, reset, and IRQ pins (optional)
+  LoRa.setPins(csPin, resetPin, irqPin);// set CS, reset, IRQ pin
+
+  if (!LoRa.begin(915E6)) {             // initialize ratio at 915 MHz
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);                       // if failed, do nothing
+  }
+
+  Serial.println("LoRa init succeeded.");
 }
 
 void loop() {
-  Serial.println("Hello, world!");
-  delay(1000);
+  if (millis() - lastSendTime > interval) {
+    String message = "HeLoRa World!";   // send a message
+    sendMessage(message);
+    Serial.println("Sending " + message);
+    lastSendTime = millis();            // timestamp the message
+    interval = random(2000) + 1000;    // 2-3 seconds
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));  
+  }
+
+  // parse for a packet, and call onReceive with the result:
+  onReceive(LoRa.parsePacket());
+}
+
+void sendMessage(String outgoing) {
+  LoRa.beginPacket();                   // start packet
+  LoRa.write(destination);              // add destination address
+  LoRa.write(localAddress);             // add sender address
+  LoRa.write(msgCount);                 // add message ID
+  LoRa.write(outgoing.length());        // add payload length
+  LoRa.print(outgoing);                 // add payload
+  LoRa.endPacket();                     // finish packet and send it
+  msgCount++;                           // increment message ID
+}
+
+void onReceive(int packetSize) {
+  if (packetSize == 0) return;          // if there's no packet, return
+
+  // read packet header bytes:
+  int recipient = LoRa.read();          // recipient address
+  byte sender = LoRa.read();            // sender address
+  byte incomingMsgId = LoRa.read();     // incoming msg ID
+  byte incomingLength = LoRa.read();    // incoming msg length
+
+  String incoming = "";
+
+  while (LoRa.available()) {
+    incoming += (char)LoRa.read();
+  }
+
+  if (incomingLength != incoming.length()) {   // check length for error
+    Serial.println("error: message length does not match length");
+    return;                             // skip rest of function
+  }
+
+  // if the recipient isn't this device or broadcast,
+  if (recipient != localAddress && recipient != 0xFF) {
+    Serial.println("This message is not for me.");
+    return;                             // skip rest of function
+  }
+
+  // if message is for this device, or broadcast, print details:
+  Serial.println("Received from: 0x" + String(sender, HEX));
+  Serial.println("Sent to: 0x" + String(recipient, HEX));
+  Serial.println("Message ID: " + String(incomingMsgId));
+  Serial.println("Message length: " + String(incomingLength));
+  Serial.println("Message: " + incoming);
+  Serial.println("RSSI: " + String(LoRa.packetRssi()));
+  Serial.println("Snr: " + String(LoRa.packetSnr()));
+  Serial.println();
 }
